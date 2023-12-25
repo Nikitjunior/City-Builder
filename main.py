e9681d5ecc3d492b4f35de6a8b9dbbebff2e718a
nplanes <nasedkin_n22@mail.ru>
2023-12-25 19:56:26 +0300

    adding hood and game currency

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sys
 
 
 def load_image(name, colorkey=None):
-    fullname = os.path.join('data_images', name)
+    fullname = os.path.join('data', name)
     if not os.path.isfile(fullname):
         print(f"Файл с изображением '{fullname}' не найден")
         sys.exit()
@@ -12,27 +12,27 @@ def load_image(name, colorkey=None):
     return image
 
 
-class Button(pygame.sprite.Sprite):
-    image = load_image("button.png")
+#class Button(pygame.sprite.Sprite):
+    #image = load_image("button.png")
 
-    def __init__(self, group, screen, x: int, y: int, width: int, height: int):
-        super().__init__(group)
-        self.screen = screen
-        self.x, self.y, self.width, self.height = x, y, width, height
+    #def __init__(self, group, screen, x: int, y: int, width: int, height: int):
+        #super().__init__(group)
+        #self.screen = screen
+        #self.x, self.y, self.width, self.height = x, y, width, height
 
-        self.image = Button.image
-        self.image = pygame.transform.scale(self.image, (width, height))
-        self.rect = self.image.get_rect()
-        self.rect = self.rect.move(self.x, self.y)
+        #self.image = Button.image
+        #self.image = pygame.transform.scale(self.image, (width, height))
+        #self.rect = self.image.get_rect()
+        #self.rect = self.rect.move(self.x, self.y)
 
-    def update(self):
-        x1, y1 = pygame.mouse.get_pos()
-        if self.x <= x1 <= self.x + self.width and self.y <= y1 <= self.y + self.height:
-            print("click")
+    #def update(self):
+        #x1, y1 = pygame.mouse.get_pos()
+        #if self.x <= x1 <= self.x + self.width and self.y <= y1 <= self.y + self.height:
+            #print("click")
 
-    def set_image(self, image):
-        self.image = load_image(image)
-        self.image = pygame.transform.scale(self.image, (self.width, self.height))
+    #def set_image(self, image):
+        #self.image = load_image(image)
+        #self.image = pygame.transform.scale(self.image, (self.width, self.height))
 
 
 class Cell:
@@ -50,7 +50,7 @@ class Building(Cell):
         self.income = 100
 
     def build(self):
-        pass    # создать объект классa Building на координатах x, y
+        pass  # создать объект классa Building на координатах x, y
 
 
 class Field(Cell):
@@ -69,13 +69,16 @@ class Field(Cell):
 
 class Board:
     def __init__(self, width: int, height: int, screen):
+        self.coins = 1000
+        self.banknotes = 50
         self.width = width
         self.height = height
         self.board = [[Field(x, y) for x in range(width)] for y in range(height)]
         self.screen = screen
-        boardfield = pygame.image.load('data_images/fon-trava.jpg') # фон
+        boardfield = load_image('fon-trava.jpg')
         screen.blit(boardfield, (0, 0))
         self.render()
+        self.hood()
         pygame.display.flip()
 
     def is_clicked(self, pos: tuple):
@@ -87,6 +90,22 @@ class Board:
             for j in range(self.width):
                 pygame.draw.rect(screen, 'white', (50 * j, 50 * i,
                                                    50, 50), 1)
+    def hood(self):
+        pygame.draw.rect(screen, 'white', (0, 0, 200, 50))
+        coinimage = load_image('coin.jpg')
+        screen.blit(coinimage, (0, 0))
+        font = pygame.font.Font(None, 30)
+        text = font.render(str(self.coins), True, (255, 217, 25))
+        screen.blit(text, (50, 25))
+        banknoteimage = load_image('banknote.jpg')
+        screen.blit(banknoteimage, (110, 0))
+        font = pygame.font.Font(None, 30)
+        text = font.render(str(self.banknotes), True, (84, 255, 159))
+        screen.blit(text, (160, 25))
+        #self.shop = Button(???)
+        #self.destruct = Button(???) Обговорим как работает класс Button
+
+
 
 
 if __name__ == '__main__':
@@ -94,8 +113,9 @@ if __name__ == '__main__':
     running = True
     pygame.init()
     pygame.display.set_caption('City Builder')
-    size = width, height = 1000, 700
+    size = width, height = 1000, 750
     screen = pygame.display.set_mode(size)
+    screen.fill((255, 255, 255))
     pygame.display.flip()
     board = Board(20, 14, screen)
     field = Field(0, 0)
