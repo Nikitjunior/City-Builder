0910919bcfa4e35379c9a29516ecfc5b69ba7da1
Nikitjunior <nikitjunior@gmail.com>
2023-12-26 03:15:17 +0300

    created a class Road

--- main.py
+++ main.py
@@ -12,10 +12,69 @@ def load_image(name, colorkey=None):
     return image
 
 
+class Road(pygame.sprite.Sprite):
+    image = load_image("roads.png")
+
+    def __init__(self, group: pygame.sprite.Group, x: int, y: int):
+        super().__init__(group)
+        self.x, self.y = x, y
+
+        self.image = Road.image.subsurface((250, 0, 250, 250))
+        self.image = pygame.transform.scale(self.image, (CELL_SIZE, CELL_SIZE))
+        self.rect = self.image.get_rect()
+        self.rect = self.rect.move(self.x * CELL_SIZE, self.y * CELL_SIZE)
+        group.update()
+
+    def update(self):
+        self.rect.move(self.x * CELL_SIZE, self.y * CELL_SIZE)
+
+        neighbours = [(self.x, y) for y in range(self.y - 1, self.y + 2) if
+                      (y != self.y and 0 <= y <= board.height - 1 and isinstance(board.board[y][self.x], Road))]
+        neighbours += [(x, self.y) for x in range(self.x - 1, self.x + 2) if
+                       (x != self.x and 0 <= x <= board.width - 1 and isinstance(board.board[self.y][x], Road))]
+
+        if 0 == len(neighbours) or len(neighbours) >= 3:
+            self.image = Road.image.subsurface((250, 0, 250, 250))
+        elif len(neighbours) == 1:
+            x, y = neighbours[0]
+            if y > self.y or y < self.y:
+                self.image = Road.image.subsurface((0, 0, 250, 250))
+            elif x > self.x or x < self.x:
+                self.image = Road.image.subsurface((0, 0, 250, 250))
+                self.image = pygame.transform.rotate(self.image, 90)
+        elif len(neighbours) == 2:
+            t1, t2 = neighbours
+            x1, y1 = t1
+            x2, y2 = t2
+            if (x1 < self.x < x2) or (x2 < self.x < x1):
+                self.image = Road.image.subsurface((0, 0, 250, 250))
+                self.image = pygame.transform.rotate(self.image, 90)
+            elif (y1 < self.y < y2) or (y2 < self.y < y1):
+                self.image = Road.image.subsurface((0, 0, 250, 250))
+            elif ((x1 > self.x and self.y == y1) and (y2 > self.y and x2 == self.x)) or (
+                    (x2 > self.x and self.y == y2) and (y1 > self.y and x1 == self.x)):
+                self.image = Road.image.subsurface((500, 0, 250, 250))
+                self.image = pygame.transform.rotate(self.image, 270)
+            elif ((x1 > self.x and self.y == y1) and (y2 < self.y and x2 == self.x)) or (
+                    (x2 > self.x and self.y == y2) and (y1 < self.y and x1 == self.x)):
+                self.image = Road.image.subsurface((500, 0, 250, 250))
+            elif ((x1 < self.x and self.y == y1) and (y2 < self.y and x2 == self.x)) or (
+                    (x2 < self.x and self.y == y2) and (y1 < self.y and x1 == self.x)):
+                self.image = Road.image.subsurface((500, 0, 250, 250))
+                self.image = pygame.transform.rotate(self.image, 90)
+            elif ((x1 < self.x and self.y == y1) and (y2 > self.y and x2 == self.x)) or (
+                    (x2 < self.x and self.y == y2) and (y1 > self.y and x1 == self.x)):
+                self.image = Road.image.subsurface((500, 0, 250, 250))
+                self.image = pygame.transform.rotate(self.image, 180)
+        self.image = pygame.transform.scale(self.image, (CELL_SIZE, CELL_SIZE))
+        self.rect = self.image.get_rect()
+        self.rect = self.rect.move(self.x * CELL_SIZE, self.y * CELL_SIZE)
+
+
 class Button(pygame.sprite.Sprite):
     image = load_image("button.png")
 
-    def __init__(self, group, screen, x: int, y: int, width: int, height: int):
+    def __init__(self, group: pygame.sprite.Group, screen, x: int, y: int, width: int, height: int):
         super().__init__(group)
         self.screen = screen
         self.x, self.y, self.width, self.height = x, y, width, height
@@ -50,7 +109,7 @@ class Building(Cell):
         self.income = 100
 
     def build(self):
-        pass    # создать объект классa Building на координатах x, y
+        pass  # создать объект классa Building на координатах x, y
 
 
 class Field(Cell):
@@ -73,20 +132,20 @@ class Board:
         self.height = height
         self.board = [[Field(x, y) for x in range(width)] for y in range(height)]
         self.screen = screen
-        boardfield = pygame.image.load('data_images/fon-trava.jpg') # фон
+        boardfield = pygame.image.load('data_images/fon-trava.jpg')  # фон
         screen.blit(boardfield, (0, 0))
         self.render()
         pygame.display.flip()
 
     def is_clicked(self, pos: tuple):
         x, y = pos
-        print(x // 50, y // 50)
+        return (x // CELL_SIZE, y // CELL_SIZE)
 
     def render(self):
         for i in range(self.height):
             for j in range(self.width):
-                pygame.draw.rect(screen, 'white', (50 * j, 50 * i,
-                                                   50, 50), 1)
+                pygame.draw.rect(screen, 'white', (CELL_SIZE * j, CELL_SIZE * i,
+                                                   CELL_SIZE, CELL_SIZE), 1)
 
 
 if __name__ == '__main__':
@@ -95,14 +154,21 @@ if __name__ == '__main__':
     pygame.init()
     pygame.display.set_caption('City Builder')
     size = width, height = 1000, 700
+    CELL_SIZE = 25
     screen = pygame.display.set_mode(size)
     pygame.display.flip()
-    board = Board(20, 14, screen)
-    field = Field(0, 0)
+    board = Board(40, 28, screen)
+    roads = pygame.sprite.Group()
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                board.is_clicked(event.pos)
+                if event.button == pygame.BUTTON_LEFT:
+                    x, y = board.is_clicked(event.pos)
+                    board.board[y][x] = Road(roads, x, y)
+                    roads.draw(screen)
+                    roads.update()
+        pygame.display.flip()
     pygame.quit()
